379102ef1555235e25288f59c88f99ec72a9f0ed
BhoomikaDua <dua.bhoomika@gail.com>
2021-03-22 12:25:48 +0530

    Add Category Functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,4 +5,20 @@ class CategoriesController < ApplicationController
 
   def new
   end
+
+  def create
+    new_category = Category.new(
+      name: params[:name],
+      is_active: params[:is_active],
+      created_on: DateTime.now
+    )
+    if new_category.save
+      redirect_to categories_path
+    else
+      flash[:error] = "The Details Filled Are Invalid, Please Try Again!"
+      redirect_to new_category_path
+    end
+
+
+  end
 end
