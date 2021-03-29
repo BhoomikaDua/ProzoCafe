f196652f688983377bb8e898220c1c29557f1bca
BhoomikaDua <dua.bhoomika@gail.com>
2021-03-29 18:39:30 +0530

    Invoices within a range edge case

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -6,8 +6,8 @@ class ReportController < ApplicationController
     start_date = params[:start_date]
     end_date = params[:end_date]
     @invoices = Invoice.where(['created_at >= ? and created_at <= ?',start_date.to_s, end_date.to_s])
-    if(@invoices.count == 0)
-      flash[:error] = "No invoices found within the range, Showing All Invoices!"
+    if(start_date == '' || end_date == '' || @invoices.count == 0)
+      flash[:error] = "No invoices found within the range(empty), Showing All Invoices!"
       redirect_to categories_path
     else
       render :template => "invoices/index"
