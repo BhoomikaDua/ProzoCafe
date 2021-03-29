ad8fa78ae0e5abd741445d1de87f3e59502aa11f
BhoomikaDua <dua.bhoomika@gail.com>
2021-03-29 18:37:05 +0530

    Invoices within a range

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,4 +1,17 @@
 class ReportController < ApplicationController
   def index
   end
+
+  def create
+    start_date = params[:start_date]
+    end_date = params[:end_date]
+    @invoices = Invoice.where(['created_at >= ? and created_at <= ?',start_date.to_s, end_date.to_s])
+    if(@invoices.count == 0)
+      flash[:error] = "No invoices found within the range, Showing All Invoices!"
+      redirect_to categories_path
+    else
+      render :template => "invoices/index"
+    end
+
+  end
 end
