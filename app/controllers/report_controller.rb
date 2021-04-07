38ccb7408df97d26af7ff259673c4b9ceefa9751
BhoomikaDua <dua.bhoomika@gail.com>
2021-04-07 15:40:07 +0530

    Refactoring Report controller

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -5,13 +5,16 @@ class ReportController < ApplicationController
   def create
     start_date = params[:start_date]
     end_date = params[:end_date]
-    date_range = "#{start_date} - #{end_date}"
-    @invoices = Invoice.where(['created_at >= ? and created_at <= ?',start_date.to_s, end_date.to_s])
 
     if(start_date == '' || end_date == '')
       flash[:error] = "Empty Date Fields!"
       redirect_back(fallback_location: "invoices/index")
-    elsif(@invoices.count == 0)
+    end
+
+    date_range = "#{start_date} - #{end_date}"
+    @invoices = Invoice.where(['created_at >= ? and created_at <= ?',start_date.to_s, end_date.to_s])
+
+    if(@invoices.count == 0)
       flash[:error] = "No invoices found between #{date_range}"
       redirect_back(fallback_location: "invoices/index")
     else
